674581c84d4d7b86ae795c3042e3bee280df88d9
Xuanzhihao <xuanzhihao1997@foxmail.com>
2020-06-10 10:34:14 +0800

    第三章结束

---
+++ src/main/java/com/example/miaosha/service/GoodsService.java
@@ -0,0 +1,29 @@
+package com.example.miaosha.service;
+
+import com.example.miaosha.dao.GoodsDao;
+import com.example.miaosha.domain.MiaoshaGoods;
+import com.example.miaosha.vo.GoodsVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GoodsService {
+    @Autowired
+    GoodsDao goodsDao;
+
+    public List<GoodsVo> listGoodsVo(){
+        return goodsDao.listGoodsVo();
+    }
+
+    public GoodsVo getGoodsVoByGoodsId(long goodsId) {
+        return goodsDao.getGoodsVoByGoodsId(goodsId);
+    }
+
+    public void reduceStock(GoodsVo goods) {
+        MiaoshaGoods g = new MiaoshaGoods();
+        g.setGoodsId(goods.getId());
+        goodsDao.reduceStock(g);
+    }
+}
