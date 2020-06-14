1b884f4148da6e89a131f08b367d94f833053a91
Xuanzhihao <xuanzhihao1997@foxmail.com>
2020-06-14 18:56:46 +0800

    第七章结束，项目完成

--- src/main/java/com/example/miaosha/config/UserArgumentResolver.java
+++ src/main/java/com/example/miaosha/config/UserArgumentResolver.java
@@ -1,6 +1,7 @@
 package com.example.miaosha.config;
 
 import com.alibaba.druid.util.StringUtils;
+import com.example.miaosha.access.UserContext;
 import com.example.miaosha.domain.MiaoshaUser;
 import com.example.miaosha.service.MiaoshaUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,28 +30,8 @@ public class UserArgumentResolver implements HandlerMethodArgumentResolver {
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-        HttpServletResponse response = webRequest.getNativeResponse(HttpServletResponse.class);
-
-        String paramToken = request.getParameter(MiaoshaUserService.COOKI_NAME_TOKEN);
-        String cookieToken = getCookieValue(request, MiaoshaUserService.COOKI_NAME_TOKEN);
-        if(StringUtils.isEmpty(cookieToken) && StringUtils.isEmpty(paramToken)) {
-            return null;
-        }
-        String token = StringUtils.isEmpty(paramToken)?cookieToken:paramToken;
-        return userService.getByToken(response, token);
+        return UserContext.getUser();
     }
 
-    private String getCookieValue(HttpServletRequest request, String cookiName) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies == null || cookies.length <= 0){
-            return null;
-        }
-        for(Cookie cookie : cookies) {
-            if(cookie.getName().equals(cookiName)) {
-                return cookie.getValue();
-            }
-        }
-        return null;
-    }
+
 }
